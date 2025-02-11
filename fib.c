a0aa2fc929064f0428cbcc2fcd90a517289c7c4a
beatle <118904398+buggedbeatle998@users.noreply.github.com>
2025-02-11 22:53:03 +0000

    finished

--- fib.c
+++ fib.c
@@ -21,60 +21,51 @@ uint32_t log2_32(uint32_t value)
     return tab32[(uint32_t)(value * 0x07C4ACDD) >> 27];
 }
 
-void fib(uint32_t n, mpz_t **output) {
-    mpz_t m0i, m1i, t0i;
-    mpz_inits(m0i, m1i, t0i);
-    mpz_set_ui(m0i, 0);
-    mpz_set_ui(m1i, 1);
-    mpz_t *m0 = &m0i;
-    mpz_t *m1 = &m1i;
-    mpz_t *t0 = &t0i;
-    
-    for (uint8_t i = log2_32(n); i >= 0, i--;) {
-        mpz_mul(*t0, *m1, *m1);
+uint32_t fib(uint32_t n) {
+    if (n > 1) {
+        mpz_t m0i, m1i, t0i;
+        mpz_inits(m0i, m1i, t0i, NULL);
+        mpz_set_ui(m0i, 0);
+        mpz_set_ui(m1i, 1);
+        clock_t before = clock();
+        mpz_t *m0 = &m0i;
+        mpz_t *m1 = &m1i;
+        mpz_t *t0 = &t0i;
+        uint8_t i = log2_32(n);
 
-        mpz_add(*m1, *m1, *m1);
-        mpz_mul(*m1, *m1, *m0);
-        mpz_add(*m1, *m1, *t0);
+        //for (uint8_t i = log2_32(n); i >= 0, i--;) {
+        while (i--) {
+            mpz_mul(*t0, *m1, *m1);
+            
+            mpz_add(*m1, *m1, *m1);
+            mpz_mul(*m1, *m1, *m0);
+            mpz_add(*m1, *m1, *t0);
 
-        mpz_mul(*m0, *m0, *m0);
-        mpz_add(*m0, *m0, *t0);
-        if (n & (1 << i)) {
-            t0 = m0;
-            m0 = m1;
-            m1 = t0;
-            mpz_add(*m1, *m1, *m0);
+            mpz_mul(*m0, *m0, *m0);
+            mpz_add(*m0, *m0, *t0);
+            if (n & (1 << i)) {
+                mpz_swap(*m0, *m1);
+                mpz_add(*m1, *m1, *m0);
+            }
         }
+
+        clock_t after = clock();
+        //gmp_printf("%Zd\n", *m1);
+        mpz_clears(m0i, m1i, t0i, NULL);
+        printf("It took %d ms to calculate the %dth fibonacci number.\n", (after - before) * 1000 / CLOCKS_PER_SEC, n);
+    } else {
+        printf("%d\n", n);
+        printf("It was Instant to calculate the %dth fibonacci number.\n", n);
+        return 0;
     }
+    
+    
 
-    printf("hello");
-    *output = m0;
-
-    printf("hello");
-    mpz_clear(m1i);
-    mpz_clear(t0i);
-    printf("hello");
 }
 
 
 int main() {
-    // FILE *filed;
-    int n = 10;
+    fib(1000000000);
 
-    clock_t before = clock();
-    mpz_t **output;
-    printf("hello");
-    fib(n, output);
-    printf("hello");
-    clock_t after = clock();
-    gmp_printf("%Zd\n", **output);
-    printf("It took %d ms to calculate the %dth fibonacci number.", (after - before) * 1000 / CLOCKS_PER_SEC, n);
-
-    // filed = fopen("fib.txt", "w");
-    // gmp_printf("%Zd", output);
-
-    mpz_clear(**output);
-    // fclose(filed);
-    
     return 0;
 }
